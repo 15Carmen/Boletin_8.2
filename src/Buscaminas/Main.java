73f441519adf6dd8312c6e234814e3d885990c9d
Carmen Martín Núñez <carmen.martin@iesnervion.es>
2022-02-23 12:55:33 +0100

    Initial commit

---
+++ src/Buscaminas/Main.java
@@ -0,0 +1,9 @@
+package Buscaminas;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/Buscaminas/Tablero.java
@@ -0,0 +1,14 @@
+package Buscaminas;
+
+public class Tablero {
+
+    //Constantes
+    public final int minas=-3;
+    public final int bandera=-7;
+    public final int numMinas=10;
+
+    //tablero
+    private final int [][] tablero =new int[8][8];
+
+
+}
